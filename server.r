0f807cf39120c21c8c0acacf83b8be5cfb6ca25d
Michel HERBIN <michel.herbin51@gmail.com>
2016-12-27 21:21:29 +0100

    version en ligne

--- server.r
+++ server.r
@@ -1,18 +1,19 @@
 library(shiny)
 require(mvtnorm)
+require(igraph)
 require(visNetwork)
 
 shinyServer(
   function(input, output){
     nb <- reactive(input$nb)
     maxsample <- reactive(input$maxsize)
-    data <- NULL
-    
+    seed <- reactive(input$seed)
+
     donnees <- reactive({
       n1 <- nb()/2
       n2 <- (nb()/5)*2
       n3 <- nb()-n1-n2
-      source <- 215
+      source <- seed()
       set.seed(source)
       data1 <- NULL
       if(as.numeric(input$distrib) == 1){
@@ -43,9 +44,7 @@ shinyServer(
     })
     
     
-    adjacente <- reactive({
-      n <- nb()
-      data <- donnees()
+    lien=function(data, n){
       distances <- as.matrix(dist(data, 
                                   diag = TRUE, upper = TRUE, method= "euclidean"))
       pointdevues <- array(rep(0, n*n), c(n, n))
@@ -57,97 +56,78 @@ shinyServer(
         ligne_j <- pointdevues[j,]
         avant_j <-  (1<ligne_i) & (ligne_i<pointdevues[i,j])
         avant_i <-  (1<ligne_j) & (ligne_j<pointdevues[j,i])
-        # retourne vrai=1 s'il n'y a exclusivement rien entre i et j
         return( !(sum(avant_i & avant_j)) ) 
       }
-      # lien logique (true ou false)
-      lien <- matrix(mapply(entre, pointdevues, row(pointdevues), col(pointdevues)), n)
-      matrix(as.numeric(lien)-diag(1,n), n)
+      lesliens <- matrix(mapply(entre, pointdevues, row(pointdevues), col(pointdevues)), n, n)
+      return(matrix(as.numeric(lesliens)-diag(1,n), n, n))
+    }
+    
+    adjacent <- reactive({
+      n <- nb()
+      data <- donnees()
+      lien(data, n)
     })
     
     
     
+    orient=function(adj, n){
+      gain<-array(rep(0, n*n), c(n, n))
+      for(i in 1:n){
+        for(j in 1:n){
+          if(adj[i, j] != 0){
+            gain[i, j]<-sum(adj[,i])
+          }
+        }
+      }
+      perte<-array(rep(0, n*n), c(n, n))
+      for(i in 1:n){
+        for(j in 1:n){
+          if(adj[i, j] != 0){
+            perte[i, j]<-sum(adj[j,])
+          }
+        }
+      }
+      direct<-adj
+      for(i in 1:n){
+        for(j in 1:n){
+          if(gain[i,j] < perte[i,j]){
+            direct[j, i]<-0
+          }
+        }
+      }
+      # second step
+      for(i in 1:n){
+        for(j in 1:n){
+          if((direct[i, j] != 0) & (gain[i,j] == perte[i,j])){
+            coord <- which(direct[,i] != 0, arr.ind=TRUE)
+            gainbis <- max(gain[coord,i])
+            coord <- which(direct[j,] != 0, arr.ind=TRUE)
+            pertebis<- max(perte[j,coord])
+            if(gainbis > pertebis){
+              direct[i, j]<-0
+            } else {
+              direct[j, i] <- 0
+            }
+          } 
+        }
+      }
+      return(matrix(direct, n, n))
+    }
+    
     gains <- reactive({
       n <- nb()
-      d <- 2
       data <- donnees()
-      op <- c(1:n)
-      echantillon <- data
       maxsize <- as.numeric(input$maxsize)
       sizesample <- n
+      selectsample <- c(1:n)
+      echantillon <- data[selectsample,]
       
       while(sizesample > maxsize){
-        
-        distances <- as.matrix(dist(echantillon, diag = TRUE, upper = TRUE, method= "euclidean"))
-        pointdevues<-array(rep(0, n*n), c(n, n))
-        for(individu in 1:n){
-          pointdevues[individu, ] <- rank(distances[individu, ], ties.method= "random")
-        }
-        entre=function(x, i, j){
-          ligne_i <- pointdevues[i,]
-          ligne_j <- pointdevues[j,]
-          avant_j <-  (1<ligne_i) & (ligne_i<pointdevues[i,j])
-          avant_i <-  (1<ligne_j) & (ligne_j<pointdevues[j,i])
-          # retourne vrai=1 s'il n'y a exclusivement rien entre i et j
-          return( !(sum(avant_i & avant_j)) ) 
-        }
-        lien <- matrix(mapply(entre, pointdevues, row(pointdevues), col(pointdevues)), n)
-        mat.adjacence <- as.numeric(lien) - diag(1,n)
-      
-        gain<-array(rep(0, n*n), c(n, n))
-        for(i in 1:n){
-          for(j in 1:n){
-            if(mat.adjacence[i, j] != 0){
-            # nombre de liens strictement entrants sur i pour le lien [i,j]
-              gain[i, j]<-sum(mat.adjacence[,i])
-            }
-          }
-        }
-        perte<-array(rep(0, n*n), c(n, n))
-        for(i in 1:n){
-          for(j in 1:n){
-            if(mat.adjacence[i, j] != 0){
-              # nombre de liens strictement sortants de j pour le lien [i,j]
-              perte[i, j]<-sum(mat.adjacence[j,])
-            }
-          }
-        }
-        mat.orient<-mat.adjacence
-        for(i in 1:n){
-          for(j in 1:n){
-            # il reste des liens non orientes en cas d'egalite
-            if(gain[i,j] < perte[i,j]){
-              # orienter vers le debit le plus fort
-              # l'autre sens devient nul
-              mat.orient[j, i]<-0
-            }
-          }
-        }
-        # orientation : 2eme passe
-        for(i in 1:n){
-          for(j in 1:n){
-            # il reste des liens non orientes en cas d'egalite
-            if((mat.orient[i, j] != 0) & (gain[i,j] == perte[i,j])){
-              # remonter en amont pour l'origine
-              # tous les liens orientes ou non, d'extremite i
-              coord <- which(mat.orient[,i] != 0, arr.ind=TRUE)
-              gainbis <- max(gain[coord,i])
-              # descendre en aval pour l'extremite
-              # tous les liens d'origine j
-              coord <- which(mat.orient[j,] != 0, arr.ind=TRUE)
-              pertebis<- max(perte[j,coord])
-              if(gainbis > pertebis){
-                mat.orient[i, j]<-0
-              } else {
-                mat.orient[j, i] <- 0
-              }
-            } 
-          }
-        }
+        mat.adjacence <- lien(echantillon, sizesample)
+        mat.direction <- orient(mat.adjacence, sizesample)
         op<-NULL
         for(i in 1:n){ 
-          # pas de sortant mais au moins un entrant
-          if( (sum(mat.orient[i,]) == 0) & (sum(mat.orient[,i]) != 0) ){
+           if( (sum(mat.direction[i,]) == 0) & (sum(mat.direction[,i]) != 0) ){
             op<-c(op, i)
           }
         }
@@ -156,35 +136,50 @@ shinyServer(
           n <- length(op)
           data <- echantillon
           echantillon <- data[op,]
+          choix <- selectsample
+          selectsample <- choix[op]
         }
       }
-      
       sample <- NULL
       if(sizesample == 1) {
         sample <- matrix(echantillon[op,], 1, 2)
+        choix <- as.vector(selectsample[op])
       } else {
         if(sizesample == 0) {
           sample <- donnees()
+          choix <- c(1:n)
         } else {
           sample <- echantillon
+          choix <- selectsample
         }
       }
-      sample
+      choix
     })
+    
     
     
     output$readme <- renderUI({
       str1 <- h4("Determination of a subsample of a 2D dataset")
       str2 <- paste( "The ", as.character(nb()), " points of a 2D dataset 
-                simulate sensors of a Wireless Sensors Network (WSN). 
-                Sub-samples could determine the location of the base stations in the network.")
+              simulate sensors of a Wireless Sensors Network. 
+              The sub-samples could determine the locations of the base stations in WSN.")
       str3 <- " "
       str4 <- "Ref.:"
+      str4b <- " "
       str5 <- "\"Using Data as Observers: a New Paradigm for Prototypes Selection\" "
-      str6 <- "M. Herbin, D. Gillard and L. Hussenet"
-      str7 <- "Innovations for Community Services (I4CS),  
-                 pp 74-82, Springer, Vienna, Austria, june 2016. "
-      HTML(paste(str1, str2, str3, str4, str5, str6, str7, sep="<br/>"))
+      str6 <- "M. Herbin, D. Gillard and L. Hussenet "
+      str7 <- "Innovations for Community Services, I4CS2016, Vienna, Austria, june 2016"
+      str7b <- " "
+      str8 <- "Publication of selected papers in
+               \"Communications in Computer and Information Science\" "
+      str9 <- "Edts: Gunter Fahrnberger, Gerald Eichler, Christian Erfurth"
+      str10 <- "Springer, pp 39-46, 2016, DOI 10.1007  978-3-319-49466-1"
+      stra <- " "
+      strb <- "Acknowledgment: partially supported by the EC SCOOP project
+               INEA/CEF/TRAN/A2014/1042281"
+      
+      HTML(paste(str1, str2, str3, str4, str4b, str5, str6, 
+                 str7, str7b, str8, str9, str10, stra, strb, sep = "<br/>" ))
       })
     
     
@@ -202,6 +197,7 @@ shinyServer(
            xlim=c(0,20), 
            ylim=c(0, 20),
            pch=c(3, 2, 1)[label], col=c("navy","darkgreen", "red3")[label],
+           label = label,
            cex=c(0.8, 0.8, 1.2)[label])
       title(main=paste("Simulation with n = ", nb()))
     })
@@ -210,18 +206,18 @@ shinyServer(
     output$skeleton <- renderPlot({
       n <- nb()
       data <- donnees()
-      mat.adjacence <- adjacente()
+      mat.adjacence <- adjacent()
 
       colnames(data) <- c("x", "y")
       plot(data, 
            xlim=c(0,20), 
            ylim=c(0, 20),
-           pch=16, col="navy",
-           cex=0.8)
+           pch=1, col="navy",
+           cex=2.5)
       title(main=paste("Skeleton of the network"))
+      text(data, labels=c(1:n), cex= 0.7)
       
       for(i in 1:(n-1)){
-        # non oriente : traitement diagonal superieure
         for(j in i:n){
            if(mat.adjacence[i, j] != 0){
             segments(data[i,1], data[i,2], data[j,1], data[j,2])
@@ -234,8 +230,9 @@ shinyServer(
     output$sampling <- renderPlot({
       data <- donnees()
       n <- nb()
-      mat.adjacence <- adjacente()
-      sample <- gains()
+      mat.adjacence <- adjacent()
+      choix <- gains()
+      sample <- matrix(data[choix,], length(choix), 2)
       
       colnames(data) <- c("x", "y")
       plot(data, 
@@ -245,19 +242,17 @@ shinyServer(
            cex=0.8)
       
       for(i in 1:(n-1)){
-        # non oriente : traitement diagonal superieure
         for(j in i:n){
           if(mat.adjacence[i, j] != 0){
             segments(data[i,1], data[i,2], data[j,1], data[j,2])
           }
         }
       }
-      
-      for(i in c(1:dim(sample)[1])){
-        points(sample[i,1], sample[i,2], lty="solid", pch=19, cex=1.5)
+      for(i in c(1:length(choix))){
+        points(sample[i,1], sample[i,2], lty="solid", pch=19, cex=1.6)
       }
-      
-      title(main=paste("Subsampling: A network with", dim(sample)[1], " base stations"))
+      text(sample, labels=choix, cex= 0.7, pos = 2)
+      title(main=paste("Subsampling: A network with", length(choix), " base stations"))
     })
     
     output$number <- renderText({
@@ -268,20 +263,40 @@ shinyServer(
     output$network <- renderVisNetwork({
       n <- nb()
       data <- donnees()
+
+      choix <- gains()
+      #sample <- matrix(data[choix,], length(choix), 2)
+      sensors <- rep("A", n)
+      sensors[choix] <- "B"
+      taille <- rep(10, n)
+      taille[choix] <- 20
+      nodes <- data.frame(id = 1:n, group=sensors, label=1:n, font.size = taille*2,
+                          title = paste0("<p><b>", 1:n,"</b><br> Sensor </p>"))
       
-      nodes <- data.frame(id = 1:n, shape="circle", label=c(1:n) )
-      
-      mat.adjacence <- as.vector(adjacente())
-      num <- which(mat.adjacence != 0)
-      x <- as.vector(((num-1)%%n)+1)
-      y <- as.vector((trunc((num-1)/n))+1)
-      origine <- x[x>y]
-      extremite <- y[x>y]
-      distances <- as.vector(dist(data, diag = TRUE, upper = TRUE, method= "euclidean"))
-      dist <- distances[num]
-      edges <- data.frame(from = origine, 
-                          to = extremite)
-      visNetwork(nodes, edges)
+      adjacence <- adjacent()
+      liens <- adjacence*upper.tri(adjacence, diag=FALSE)
+      num <- which(liens != 0)
+      origine <- as.vector(((num-1)%%n)+1)
+      extremite <- as.vector((trunc((num-1)/n))+1)
+      distances <- as.matrix(dist(data, diag = TRUE, upper = TRUE, method= "euclidean"))
+      distselect <- sprintf("w = %.2f", distances[num])
+      edges <- data.frame(from = origine,
+                          to = extremite,
+                          label = distselect,
+                          color = "lightgreen",
+                          width = (4*distances[num]))
+      #visNetwork(nodes, edges)  with visIgraphLayout() 
+      visNetwork(nodes, edges) %>% 
+      visGroups(groupname = "A", 
+                color = list(background = "lightblue", 
+                             border = "darkblue",
+                            highlight = "yellow"),
+                shape = "circle") %>% 
+      visGroups(groupname = "B",
+                color = list(background = "lightsalmon", 
+                             border = "darkred",
+                             highlight = "yellow"),
+                shape = "circle")    
     })
 
   }
--- ui.r
+++ ui.r
@@ -2,35 +2,41 @@ library(shiny)
 require(visNetwork)
 
 shinyUI(fluidPage(
-    titlePanel(title="Subsampling in 2D dataset"),
+    titlePanel(title="Selection of prototypes in 2D dataset with sub-sampling"),
     sidebarLayout(
       sidebarPanel(
         h3("Parameters:"),
+        sliderInput("seed",
+                    "Random generator seed:",
+                    min = 1,
+                    max = 100,
+                    value = 50,
+                    step = 1),
         sliderInput("nb",
-                    "Number of points:",
-                    min = 20,
-                    max = 500,
-                    value = 100,
-                    step = 20),
+                    "Number of sensors:",
+                    min = 10,
+                    max = 150,
+                    value = 30,
+                    step = 10),
         radioButtons("distrib", 
-                     "Kind of overlapped distributions:", 
+                     "Select spatial distributions:", 
                      list("Uniform"=1, "Gaussian"=2), 
                      1),
         sliderInput("maxsize",
-                    "Maximum of sub-samples:",
+                    "Maximum of base stations",
                     min = 1,
-                    max = 50,
+                    max = 20,
                     value = 5,
                     step = 1)
       ),
       mainPanel(
-        h2("Selection of prototypes in a 2D dataset"), 
+        h2("Base Stations of a Wireless Sensor Network"), 
         tabsetPanel(type="tab",
-                    tabPanel("Plot", plotOutput("distribution") ),
+                    tabPanel("Spatial Distribution of Sensors", plotOutput("distribution") ),
                     tabPanel("Skeleton", plotOutput("skeleton") ),
-                    tabPanel("Sampling", plotOutput("sampling"), textOutput("number") ),
-                    tabPanel("VisNetwork", visNetworkOutput("network")),
-                    tabPanel("Read me", htmlOutput("readme"))
+                    tabPanel("Base Stations", plotOutput("sampling"), textOutput("number") ),
+                    tabPanel("Network", visNetworkOutput("network") ),
+                    tabPanel("Read me", htmlOutput("readme") )
         )
         
       )
